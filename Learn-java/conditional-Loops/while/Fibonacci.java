6cdc4de25072e82684a8cbb5a102ca3c060a7350
Dharshini ramesh <165536218+dharshiniRD22@users.noreply.github.com>
2025-07-02 19:05:32 +0530

    Add files via upload

---
+++ Learn-java/conditional-Loops/while/Fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Fibonacci {
+    public static void main(String[] args) {
+        try(Scanner input = new Scanner(System.in)){
+            int n = input.nextInt();
+            int prev = 0;
+            int i = 1;
+            int count = 2;
+            while(count<=n){
+                int temp = i;
+                i = i+prev;
+                prev=temp;
+                count++;
+            }
+            System.out.println(i);
+        }
+    }
+}
