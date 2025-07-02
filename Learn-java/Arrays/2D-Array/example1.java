72f02c23ef85f8b085be65abc8cd10c3dae3fb5a
Dharshini ramesh <165536218+dharshiniRD22@users.noreply.github.com>
2025-07-02 19:20:26 +0530

    Add files via upload

---
+++ Learn-java/Arrays/2D-Array/example1.java
@@ -0,0 +1,10 @@
+
+import java.util.Arrays;
+
+public class example1{
+    public static void main(String[] args) {
+        int[][]arr = new int [3][];
+        int [][]arr1={{0,1},{2,3,4},{5}};
+        System.out.println(Arrays.deepToString(arr1));
+    }
+}
